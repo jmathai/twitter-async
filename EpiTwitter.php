92d82fbe6fcf054ac490637e934ef185d6ea5e36
danieljonesorg <git@danieljones.org>
2014-01-07 13:59:38 -0600

    Update EpiTwitter.php
    Use https for api url.

--- EpiTwitter.php
+++ EpiTwitter.php
@@ -18,7 +18,7 @@ class EpiTwitter extends EpiOAuth
   protected $accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
   protected $authorizeUrl   = 'https://api.twitter.com/oauth/authorize';
   protected $authenticateUrl= 'https://api.twitter.com/oauth/authenticate';
-  protected $apiUrl         = 'http://api.twitter.com';
+  protected $apiUrl         = 'https://api.twitter.com';
   protected $userAgent      = 'EpiTwitter (http://github.com/jmathai/twitter-async/tree/)';
   protected $apiVersion     = '1.1';
   protected $isAsynchronous = false;
